6e37f26407ce534c02f76a8c8ef34c45fb3ac36f
Martyna <m.smolarek19@gmail.com>
2023-10-05 20:45:05 +0200

    burger menu behaviour

--- src/scripts/responsiveMenu.js
+++ src/scripts/responsiveMenu.js
@@ -1,2 +1,11 @@
-const menu = document.querySelector('.burger-menu')
-const closeBtn = document.querySelector(".responsive-close")
+const menu = document.getElementById('burger-menu')
+const closeBtn = document.getElementById("closeBtn")
+const responsiveLinks = document.getElementById('responsive-links')
+
+closeBtn.addEventListener('click', () => {
+    responsiveLinks.classList.add('hidden')
+})
+
+menu.addEventListener('click', () => {
+    responsiveLinks.classList.remove('hidden')
+})
