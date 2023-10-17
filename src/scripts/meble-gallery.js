515b34ba21b648368a294b94dbc5c75b538a5887
Martyna <m.smolarek19@gmail.com>
2023-10-17 21:47:57 +0200

    update gallery styling

--- src/scripts/meble-gallery.js
+++ src/scripts/meble-gallery.js
@@ -5,7 +5,7 @@ const galleryContainer = document.getElementById('galeria-meble')
 
 const renderGallery = () => {mebleLista.forEach(image => {
     const imageContainer = document.createElement('div')
-    imageContainer.innerHTML = `<img class="h-auto max-w-full rounded-lg cursor-pointer" src="${image.imgSrc}" alt="${image.imgAlt}" />`
+    imageContainer.innerHTML = `<img class="h-auto max-w-full cursor-pointer" src="${image.imgSrc}" alt="${image.imgAlt}" />`
 
     galleryContainer.appendChild(imageContainer)
 })
