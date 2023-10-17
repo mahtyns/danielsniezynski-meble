83d474e6e52b7636194afaa97a4eeb04532fed97
Martyna <m.smolarek19@gmail.com>
2023-10-17 21:28:41 +0200

    furniture gallery

---
+++ src/assets/data/meble-galeria.js
@@ -0,0 +1,39 @@
+import img1 from '../images/gallery/kuchnia1.jpg'
+import img2 from '../images/gallery/kuchnia2.jpg'
+import img3 from '../images/gallery/kuchnia3.jpg'
+import img4 from '../images/gallery/kuchnia4.jpg'
+import img5 from '../images/gallery/lazienka.jpg'
+import img6 from '../images/gallery/meble.jpg'
+
+export const mebleLista = [
+    {
+        id: 0,
+        imgSrc: img1,
+        imgAlt: 'Kuchnia 1'
+    },
+    {
+        id: 1,
+        imgSrc: img2,
+        imgAlt: 'Kuchnia 2'
+    },
+    {
+        id: 2,
+        imgSrc: img3,
+        imgAlt: 'Kuchnia 2'
+    },
+    {
+        id: 3,
+        imgSrc: img4,
+        imgAlt: 'Kuchnia 2'
+    },
+    {
+        id: 4,
+        imgSrc: img5,
+        imgAlt: 'Kuchnia 2'
+    },
+    {
+        id: 5,
+        imgSrc: img6,
+        imgAlt: 'Kuchnia 2'
+    },
+]
---
+++ src/scripts/meble-gallery.js
@@ -0,0 +1,14 @@
+import { mebleLista } from "../assets/data/meble-galeria";
+
+const galleryContainer = document.getElementById('galeria-meble')
+
+
+const renderGallery = () => {mebleLista.forEach(image => {
+    const imageContainer = document.createElement('div')
+    imageContainer.innerHTML = `<img class="h-auto max-w-full rounded-lg" src="${image.imgSrc}" alt="${image.imgAlt}" />`
+
+    galleryContainer.appendChild(imageContainer)
+})
+}
+
+renderGallery()
