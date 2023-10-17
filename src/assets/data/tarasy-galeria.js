968b0b11f2b897aced9ae1e59b5464af8ba1a1bc
Martyna <m.smolarek19@gmail.com>
2023-10-17 21:46:02 +0200

    terrace gallery

---
+++ src/assets/data/tarasy-galeria.js
@@ -0,0 +1,23 @@
+import img1 from '../images/gallery/taras1.jpg'
+import img2 from '../images/gallery/taras2.jpg'
+import img3 from '../images/gallery/taras3.jpg'
+
+export const tarasyLista = [ 
+    {
+        id: 0,
+        imgSrc: img1,
+        imgAlt: 'Taras'
+    },
+    {
+        id: 1,
+        imgSrc: img2,
+        imgAlt: 'Taras'
+    },
+    {
+        id: 2,
+        imgSrc: img3,
+        imgAlt: 'Taras'
+    },
+
+
+]
--- src/scripts/tarasy-gallery.js
+++ src/scripts/tarasy-gallery.js
@@ -1,9 +1,9 @@
-import { mebleLista } from "../assets/data/meble-galeria";
+import { tarasyLista } from "../assets/data/tarasy-galeria";
 
-const galleryContainer = document.getElementById('galeria-meble')
+const galleryContainer = document.getElementById('galeria-tarasy')
 
 
-const renderGallery = () => {mebleLista.forEach(image => {
+const renderGallery = () => {tarasyLista.forEach(image => {
     const imageContainer = document.createElement('div')
     imageContainer.innerHTML = `<img class="h-auto max-w-full rounded-lg cursor-pointer" src="${image.imgSrc}" alt="${image.imgAlt}" />`
 
