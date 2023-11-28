6ef0e36b42ca837db1a01f1872b8a9994da3f396
Martyna <m.smolarek19@gmail.com>
2023-11-28 17:21:20 +0100

    update gallery - tarasy

--- src/scripts/tarasy-gallery.js
+++ src/scripts/tarasy-gallery.js
@@ -5,7 +5,7 @@ const galleryContainer = document.getElementById('galeria-tarasy')
 const renderGallery = () => {
     tarasyLista.forEach(image => {
         const imageContainer = document.createElement('div')
-        imageContainer.innerHTML = `<img loading="lazy" class="h-full max-w-full cursor-pointer hover:brightness-50" src="${image.imgSrc}" alt="${image.imgAlt}" />`
+        imageContainer.innerHTML = `<img loading="lazy" class="w-full h-[450px] cursor-pointer hover:brightness-50" src="${image.imgSrc}" alt="${image.imgAlt}" />`
 
         galleryContainer.appendChild(imageContainer)
     })
