98ed8b52d62bb9d4198671215bc997f21b6494e8
Martyna <m.smolarek19@gmail.com>
2023-11-28 16:59:59 +0100

    furniture gallery update

--- src/assets/data/meble-galeria.js
+++ src/assets/data/meble-galeria.js
@@ -1,39 +1,52 @@
-import img1 from '../images/gallery/kuchnia1.jpg'
-import img2 from '../images/gallery/kuchnia2.jpg'
-import img3 from '../images/gallery/kuchnia3.jpg'
-import img4 from '../images/gallery/kuchnia4.jpg'
-import img5 from '../images/gallery/lazienka.jpg'
-import img6 from '../images/gallery/meble.jpg'
+// import img1 from '../images/gallery/kuchnia1.jpg'
+// import img2 from '../images/gallery/kuchnia2.jpg'
+// import img3 from '../images/gallery/kuchnia3.jpg'
+// import img4 from '../images/gallery/kuchnia4.jpg'
+// import img5 from '../images/gallery/lazienka.jpg'
+// import img6 from '../images/gallery/meble.jpg'
 
-export const mebleLista = [
-    {
-        id: 0,
-        imgSrc: img1,
-        imgAlt: 'Kuchnia 1'
-    },
-    {
-        id: 1,
-        imgSrc: img2,
-        imgAlt: 'Kuchnia 2'
-    },
-    {
-        id: 2,
-        imgSrc: img3,
-        imgAlt: 'Kuchnia 2'
-    },
-    {
-        id: 3,
-        imgSrc: img4,
-        imgAlt: 'Kuchnia 2'
-    },
-    {
-        id: 4,
-        imgSrc: img5,
-        imgAlt: 'Kuchnia 2'
-    },
-    {
-        id: 5,
-        imgSrc: img6,
-        imgAlt: 'Kuchnia 2'
-    },
-]
+// export const mebleLista = [
+//     {
+//         id: 0,
+//         imgSrc: '/src/assets/images/gallery/kuchnia1.jpg',
+//         imgAlt: 'Kuchnia 1'
+//     },
+//     {
+//         id: 1,
+//         imgSrc: img2,
+//         imgAlt: 'Kuchnia 2'
+//     },
+//     {
+//         id: 2,
+//         imgSrc: img3,
+//         imgAlt: 'Kuchnia 2'
+//     },
+//     {
+//         id: 3,
+//         imgSrc: img4,
+//         imgAlt: 'Kuchnia 2'
+//     },
+//     {
+//         id: 4,
+//         imgSrc: img5,
+//         imgAlt: 'Kuchnia 2'
+//     },
+//     {
+//         id: 5,
+//         imgSrc: img6,
+//         imgAlt: 'Kuchnia 2'
+//     },
+// ]
+
+const filesNum = 12;
+const arrayFiles = [];
+
+for ( let i = 0; i < filesNum; i++) {
+    arrayFiles.push({
+        id: i,
+        imgSrc: `/src/assets/images/gallery/img${i+1}.jpg`,
+        imgAlt: `Realizacja ${i+1}`
+    })
+}
+
+export default arrayFiles
--- src/scripts/meble-gallery.js
+++ src/scripts/meble-gallery.js
@@ -1,11 +1,11 @@
-import { mebleLista } from "../assets/data/meble-galeria";
+import arrayFiles from "../assets/data/meble-galeria";
 
 const galleryContainer = document.getElementById('galeria-meble')
 
 
-const renderGallery = () => {mebleLista.forEach(image => {
+const renderGallery = () => {arrayFiles.slice(0,6).forEach(image => {
     const imageContainer = document.createElement('div')
-    imageContainer.innerHTML = `<img loading="lazy" class="h-auto max-w-full cursor-pointer hover:brightness-50" src="${image.imgSrc}" alt="${image.imgAlt}" />`
+    imageContainer.innerHTML = `<img loading="lazy" class="w-full h-[450px] cursor-pointer hover:brightness-50 object-cover" src="${image.imgSrc}" alt="${image.imgAlt}" />`
 
     galleryContainer.appendChild(imageContainer)
 })
@@ -15,3 +15,5 @@ renderGallery()
 
 const showMoreButton = document.getElementById('meble-see-more')
 
+const importAllImages = (context) => context.keys().map(context);
+const galleryImages = importAllImages(require.context('../assets/images/gallery', false, /\.(png|jpe?g|svg)$/));
