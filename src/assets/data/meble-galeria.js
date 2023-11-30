5c799dcecd970fa4b750fece2ec124197d5b6c4b
Martyna <m.smolarek19@gmail.com>
2023-11-30 13:12:01 +0100

    img

--- src/assets/data/meble-galeria.js
+++ src/assets/data/meble-galeria.js
@@ -4,7 +4,7 @@ const arrayFiles = [];
 for ( let i = 0; i < filesNum; i++) {
     arrayFiles.push({
         id: i,
-        imgSrc: import.meta.env.BASE_URL + `/gallery/img${i+1}.jpg`,
+        imgSrc: `../gallery/img${i+1}.jpg`,
         imgAlt: `Realizacja ${i+1}`
     })
 }
