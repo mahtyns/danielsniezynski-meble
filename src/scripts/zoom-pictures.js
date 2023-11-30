786e298c2ccc1fde5f8f9856282db3300b94ff11
Martyna <m.smolarek19@gmail.com>
2023-11-30 13:40:46 +0100

    element superposition

--- src/scripts/zoom-pictures.js
+++ src/scripts/zoom-pictures.js
@@ -10,7 +10,7 @@ const zoomInPictures = (pic) => {
     const listClasses = ['bg-black-opacity', 'w-full', 'h-full', 'z-10', 'fixed', 'top-0', 'left-0', 'flex', 'items-center', 'justify-center']
     page.classList.add('overflow-y-hidden')
     zoomInWrapper.classList.add(...listClasses);
-    zoomInWrapper.innerHTML = `<img src=${imgSrc} alt=${imgAlt} class="w-[90%] h-auto cursor-pointer" />`
+    zoomInWrapper.innerHTML = `<img src=${imgSrc} alt=${imgAlt} class="w-[90%] h-auto cursor-pointer z-30" />`
     page.insertBefore(zoomInWrapper, pageFirstChild);
     zoomInWrapper.addEventListener('click', ()=> {
         page.classList.remove('overflow-y-hidden');
