0766da39309c056457d815377d5e2ed4fd0cc475
Martyna <m.smolarek19@gmail.com>
2023-11-30 12:35:32 +0100

    cleanup

--- src/scripts/meble-gallery.js
+++ src/scripts/meble-gallery.js
@@ -14,7 +14,6 @@ const renderGallery = () => {
         .forEach(image => {
             const imageContainer = document.createElement('div')
             imageContainer.innerHTML = `<img loading="lazy" class="w-full h-[450px] cursor-pointer hover:brightness-90 object-cover" src="${image.imgSrc}" alt="${image.imgAlt}" />`
-
             galleryContainer.appendChild(imageContainer)
         })
 }
