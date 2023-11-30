384e9549eb54bbf51f2e5adeaf3351ba9b6a85a5
Martyna <m.smolarek19@gmail.com>
2023-11-30 13:34:52 +0100

    conflicts solution

--- src/assets/data/meble-galeria.js
+++ src/assets/data/meble-galeria.js
@@ -4,11 +4,7 @@ const arrayFiles = [];
 for ( let i = 0; i < filesNum; i++) {
     arrayFiles.push({
         id: i,
-<<<<<<< HEAD
-        imgSrc: `../gallery/img${i+1}.jpg`,
-=======
         imgSrc: `/src/assets/images/gallery/img${i+1}.jpg`,
->>>>>>> parent of 0b245c7... image build
         imgAlt: `Realizacja ${i+1}`
     })
 }
