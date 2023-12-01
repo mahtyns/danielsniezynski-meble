686f502ba95c1de316ba86034df2d8a18582641d
Martyna <m.smolarek19@gmail.com>
2023-12-01 12:09:49 +0100

    image aspect change

--- src/scripts/meble-gallery.js
+++ src/scripts/meble-gallery.js
@@ -5,24 +5,6 @@ const galleryContainer = document.getElementById('galeria-meble')
 const startInd = 0;
 let sliceNum = 6;
 const addMorePhotosNum = 6;
-// const page = document.querySelector('body')
-// const pageFirstChild = page.firstChild;
-
-// const zoomInPictures = (pic) => {
-//     const zoomInWrapper = document.createElement('div');
-//     const imgSrc = pic.getAttribute('src');
-//     const imgAlt = pic.getAttribute('alt');
-//     const listClasses = ['bg-black-opacity', 'w-full', 'h-full', 'z-10', 'fixed', 'top-0', 'left-0', 'flex', 'items-center', 'justify-center']
-//     page.classList.add('overflow-y-hidden')
-//     zoomInWrapper.classList.add(...listClasses);
-//     zoomInWrapper.innerHTML = `<img src=${imgSrc} alt=${imgAlt} class="w-[90%] h-auto cursor-pointer z-30" />`
-//     page.insertBefore(zoomInWrapper, pageFirstChild);
-//     zoomInWrapper.addEventListener('click', ()=> {
-//         page.classList.remove('overflow-y-hidden');
-//         zoomInWrapper.remove()
-//         zoomInWrapper.removeEventListener('click', closeZoomIn);
-//     })
-// }
 
 const renderGallery = () => {
     galleryContainer.innerHTML = '';
--- src/scripts/zoom-pictures.js
+++ src/scripts/zoom-pictures.js
@@ -7,7 +7,7 @@ export const zoomInPictures = (pic) => {
     const listClasses = ['bg-black-opacity', 'w-full', 'h-full', 'z-10', 'fixed', 'top-0', 'left-0', 'flex', 'items-center', 'justify-center']
     page.classList.add('overflow-y-hidden')
     zoomInWrapper.classList.add(...listClasses);
-    zoomInWrapper.innerHTML = `<img src=${imgSrc} alt=${imgAlt} class="w-[90%] h-auto cursor-pointer z-30" />`
+    zoomInWrapper.innerHTML = `<img src=${imgSrc} alt=${imgAlt} class="w-auto h-[70%] object-cover cursor-pointer z-30" />`
     page.insertBefore(zoomInWrapper, pageFirstChild);
     
     zoomInWrapper.addEventListener('click', ()=> {
