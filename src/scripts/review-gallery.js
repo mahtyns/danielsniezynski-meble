812599365383ec286cf7c0c53541b2a2bf4630bb
Martyna <m.smolarek19@gmail.com>
2023-12-05 12:11:51 +0100

    update main index and review script

--- src/scripts/review-gallery.js
+++ src/scripts/review-gallery.js
@@ -1,37 +1,37 @@
 let slideIndex = 0;
+const dots = document.querySelectorAll('.dot');
+const reviewSlides = document.querySelectorAll('.slides');
+let intervalId;
 
 const showReviews = (n) => {
-    const reviewSlides = document.querySelectorAll('.slides');
-    const dots = document.querySelectorAll('.dot');
-    let i;
-
-    for (i = 0; i < reviewSlides.length; i++) {
+    for (let i = 0; i < reviewSlides.length; i++) {
         reviewSlides[i].classList.add('hidden');
-    }
-    for (i = 0; i < dots.length; i++) {
         dots[i].classList.remove('bg-main-black');
     }
 
-    slideIndex++;
-    if (slideIndex > reviewSlides.length) {
-        slideIndex = 1;
+    slideIndex = n; 
+
+    if (slideIndex >= reviewSlides.length) {
+        slideIndex = 0; 
     }
 
-    reviewSlides[slideIndex - 1].classList.remove('hidden');
-    dots[slideIndex - 1].classList.add('bg-main-black');
+    reviewSlides[slideIndex].classList.remove('hidden');
+    dots[slideIndex].classList.add('bg-main-black');
 
-    // if (n > reviewSlides.length) {slideIndex = 1}
-    // if (n < 1) {slideIndex = reviewSlides.length}
-    // for (i = 0; i < reviewSlides.length; i++) {
-    // reviewSlides[i].classList.add('hidden');
-    // }
-    // for (i = 0; i < dots.length; i++) {
-    // dots[i].classList.remove('bg-main-black');
-    // }
-    // reviewSlides[slideIndex-1].style.display = "block";
-    // dots[slideIndex-1].classList.add('bg-main-black');
+    slideIndex++; 
 }
 
 showReviews(slideIndex);
 
-setInterval(showReviews, 4000);
+intervalId = setInterval(() => {
+    showReviews(slideIndex);
+}, 4000);
+
+dots.forEach((dot, index) => dot.addEventListener('click', () => {
+    console.log(index);
+    clearInterval(intervalId); 
+    showReviews(index);
+    intervalId = setInterval(() => {
+        showReviews(slideIndex);
+    }, 3000);
+}));
